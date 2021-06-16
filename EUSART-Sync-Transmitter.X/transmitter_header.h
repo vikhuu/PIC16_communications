d098f08452590f138489c150887fa8091cb40f68
Vi Khuu <khuuducvi@gmail.com>
2021-06-16 21:43:48 +0700

    Add EUSART_test module

---
+++ EUSART-Sync-Transmitter.X/transmitter_header.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   transmitter_header.h
+ * Author: AD
+ *
+ * Created on June 16, 2021, 8:46 PM
+ */
+
+#ifndef TRANSMITTER_HEADER_H
+#define	TRANSMITTER_HEADER_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* TRANSMITTER_HEADER_H */
+
---
+++ EUSART-Sync-Transmitter.X/transmitter_main.c
@@ -0,0 +1,14 @@
+/*
+ * File:   transmitter_main.c
+ * Author: AD
+ *
+ * Created on June 16, 2021, 8:46 PM
+ */
+
+
+#include <xc.h>
+
+void main(void)
+{
+    return;
+}
